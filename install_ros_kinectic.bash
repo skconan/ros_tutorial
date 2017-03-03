498886fab7eafbdc470ff2f48333e5dae37148d3
supakit.kr@gmail.com <supakit.kr@gmail.com>
2017-03-03 22:56:35 +0700

    add install new package

--- install_ros_kinectic.bash
+++ install_ros_kinectic.bash
@@ -4,8 +4,8 @@ echo "Install ROS Kinetic"
 read -r -p "Are you sure? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
-	echo "======== Install ROS Kinetic. ========";
-	sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list';
+    echo "======== Install ROS Kinetic. ========";
+    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list';
     sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116;
     sudo apt-get update;
     sudo apt-get install ros-kinetic-desktop-full;
@@ -16,5 +16,9 @@ then
     echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc;
     source ~/.bashrc;
     sudo apt-get install python-rosinstall
+    sudo apt-get install ros-kinetic-joy
+    sudo apt-get install ros-kinetic-hector-sensors-description 
+    sudo ./ueyesdk-setup-4.81.01-*	
     echo "======== Finish Install. ========";
+
 fi	
